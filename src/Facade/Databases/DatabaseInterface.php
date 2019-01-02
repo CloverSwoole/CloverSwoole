884ef702d6101f3c65a93f59f77ddc207d55da31
戒尺 <itxiao6@qq.com>
2019-01-02 11:05:12 +0800

    db 服务接口约束

--- src/Facade/Databases/DatabaseInterface.php
+++ src/Facade/Databases/DatabaseInterface.php
@@ -1,11 +1,13 @@
 <?php
 namespace Itxiao6\Framework\Facade\Databases;
+use Illuminate\Container\Container;
+
 /**
- *
+ * 数据库服务
  * Interface Database
  * @package Itxiao6\Framework\Facade\Databases
  */
 interface DatabaseInterface
 {
-
+    public static function boot(?Container $container = null);
 }
