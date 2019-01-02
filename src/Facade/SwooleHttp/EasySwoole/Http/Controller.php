1f5557218a19fab26f9e30a8905357ab55fd38d0
戒尺 <itxiao6@qq.com>
2019-01-02 13:25:26 +0800

    class 找不到 问题修复

--- src/Facade/SwooleHttp/EasySwoole/Http/Controller.php
+++ src/Facade/SwooleHttp/EasySwoole/Http/Controller.php
@@ -1,6 +1,7 @@
 <?php
 namespace Itxiao6\Framework\Facade\SwooleHttp\EasySwoole\Http;
 
+use EasySwoole\Http\Message\Status;
 use EasySwoole\Http\Request;
 use EasySwoole\Http\Response;
 use Illuminate\Container\Container;
@@ -18,4 +19,49 @@ class Controller extends \EasySwoole\Http\AbstractInterface\Controller
     }
 
     public function index(){}
+    /**
+     * 返回JSON数据
+     * @param $data
+     * @param bool $is_end
+     */
+    protected function ReturnJosn($data,$is_end = false)
+    {
+        /**
+         * 判断请求是否已经结束
+         */
+        if($this -> response() -> isEndResponse()){return ;}
+        /**
+         * 响应数据
+         */
+        $this->response()->write(json_encode($data,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
+        /**
+         * 更改协议头
+         */
+        $this->response()->withHeader('Content-type','application/json;charset=utf-8');
+        /**
+         * 允许所有跨域请求
+         */
+        $this->response()->withHeader('Access-Control-Allow-Origin','*');
+        /**
+         * 允许跨域的方法
+         */
+        $this->response()->withHeader('Access-Control-Allow-Method','POST');
+        /**
+         * 允许修改的协议头
+         */
+        $this->response()->withHeader('Access-Control-Allow-Headers','accept, content-type');
+        /**
+         * 响应码
+         */
+        $this->response()->withStatus(Status::CODE_OK);
+        /**
+         * 判断是否要结束请求
+         */
+        if($is_end){$this -> EndResponse();}
+    }
+    protected function gc()
+    {
+        parent::gc();
+//        var_dump('class :'.static::class.' is recycle to pool');
+    }
 }
