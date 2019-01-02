5dd025cb8720753262e0740ab0928a3d4e64717c
戒尺 <itxiao6@qq.com>
2019-01-02 11:04:50 +0800

    服务启动逻辑分担

--- src/Facade/Databases/Database.php
+++ src/Facade/Databases/Database.php
@@ -1,13 +1,36 @@
 <?php
 namespace Itxiao6\Framework\Facade\Databases;
+use Illuminate\Container\Container;
 use Illuminate\Database\Capsule\Manager;
 
 /**
- * 数据库门面
+ * 数据库服务
  * Class Database
  * @package Itxiao6\Framework\Facade\Databases
  */
-class Database extends Manager
+class Database
 {
-
+    /**
+     * 启动服务
+     * @param Container|null $container
+     */
+    public static function boot(?Container $container = null)
+    {
+        /**
+         * 实例化组件
+         */
+        $Manager = new Manager($container);
+        /**
+         * 注入配置
+         */
+        $Manager -> addConnection($container['config']['database']);
+        /**
+         * 设置全局访问
+         */
+        $Manager -> setAsGlobal();
+        /**
+         * 启动组件
+         */
+        $Manager -> bootEloquent();
+    }
 }
