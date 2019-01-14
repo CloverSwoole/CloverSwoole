472bc19e42dce2550dfe087a9af8560d85b8af15
戒尺 <itxiao6@qq.com>
2019-01-14 16:52:59 +0800

    Class CookieItem

---
+++ src/Facade/Http/CookieItem.php
@@ -0,0 +1,56 @@
+<?php
+namespace Itxiao6\Framework\Facade\Http;
+
+/**
+ * Class CookieItem
+ * @package Itxiao6\Framework\Facade\Http
+ */
+class CookieItem
+{
+    protected $name = '';
+    protected $value = '';
+    protected $expire = '';
+    protected $path = '';
+    protected $domain = '';
+    protected $secure = '';
+    protected $httponly = '';
+
+    public function __construct($name,$value,$expire = 0,$path = '/',$domain='',$secure=false,$httponly=false)
+    {
+        $this -> name = $name;
+        $this -> value = $value;
+        $this -> expire = $expire;
+        $this -> path = $path;
+        $this -> domain = $domain;
+        $this -> secure = $secure;
+        $this -> httponly = $httponly;
+    }
+    public function getExpire()
+    {
+        return $this -> expire;
+    }
+    public function getPath()
+    {
+        return $this -> path;
+    }
+    public function getDemain()
+    {
+        return $this -> domain;
+    }
+    public function getSecure()
+    {
+        return $this -> secure;
+    }
+    public function getHttpoly()
+    {
+        return $this -> httponly;
+    }
+    public function getName()
+    {
+        return $this -> name;
+    }
+    public function getValue()
+    {
+        return $this -> value;
+    }
+}
