dbade809bfdc8348a3d1140e253bce6c29856c08
戒尺 <itxiao6@qq.com>
2019-01-14 15:08:01 +0800

    Http 找不到控制器异常 及操作找不到异常

---
+++ src/Facade/Http/Exception/NotFoundController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Itxiao6\Framework\Facade\Http\Exception;
+
+/**
+ * 找不到控制器
+ * Class NotFoundController
+ * @package Itxiao6\Framework\Facade\Http\Exception
+ */
+class NotFoundController extends RouteBaseException
+{
+
+}
---
+++ src/Facade/Http/Exception/RouteBaseException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Itxiao6\Framework\Facade\Http\Exception;
+use Itxiao6\Framework\Exception;
+
+/**
+ * 路由组件基础异常
+ * Class RouteBaseException
+ * @package Itxiao6\Framework\Facade\Http\Exception
+ */
+class RouteBaseException extends Exception
+{
+
+}
