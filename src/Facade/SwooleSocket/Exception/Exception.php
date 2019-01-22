b5c8147b8855f6f26fd236639ba546b056d224d8
戒尺 <itxiao6@qq.com>
2019-01-22 15:30:35 +0800

    Socket 异常

---
+++ src/Facade/SwooleSocket/Exception/Exception.php
@@ -0,0 +1,12 @@
+<?php
+namespace CloverSwoole\CloverSwoole\Facade\SwooleSocket\Exception;
+
+/**
+ * Socket Exception
+ * Class Exception
+ * @package CloverSwoole\CloverSwoole\Facade\SwooleSocket\Exception
+ */
+class Exception extends \CloverSwoole\CloverSwoole\Exception
+{
+
+}
