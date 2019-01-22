027524d35ea123da19d556112afc0512a68f5693
戒尺 <itxiao6@qq.com>
2019-01-22 16:05:50 +0800

    Socket Client 实例

---
+++ src/Facade/Socket/SocketClient.php
@@ -0,0 +1,57 @@
+<?php
+namespace CloverSwoole\CloverSwoole\Facade\Socket;
+
+use Illuminate\Container\Container;
+
+/**
+ * Class SocketClient
+ * @package CloverSwoole\CloverSwoole\Facade\Socket
+ */
+abstract class SocketClient
+{
+    /**
+     * 静态全局实例
+     * @var null | SocketClient
+     */
+    protected static $interface = null;
+    /**
+     * 是否开启全局访问
+     * @var null | bool
+     */
+    protected static $global_response = null;
+    /**
+     * 获取接口
+     * @return SocketClient|null
+     */
+    protected static function getInterface()
+    {
+        return self::$interface;
+    }
+    /**
+     * 设置全局访问
+     * @param $bool
+     */
+    public function setAsGlobal($bool = true)
+    {
+        if($bool){
+            static::$global_response = $this;
+        }else{
+            static::$global_response = null;
+        }
+    }
+    /**
+     * 推送数据
+     * @param int $fd
+     * @param string|mixed $data
+     * @param int $opcode
+     * @param bool $finish
+     */
+    abstract function push(int $fd,$data);
+
+    /**
+     * 客户端是否存在
+     * @param int $fd
+     * @return bool
+     */
+    abstract public function exist(int $fd);
+}
