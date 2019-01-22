583a5ec94ed7c41da6d3f971029cc25da81f17ac
戒尺 <itxiao6@qq.com>
2019-01-22 15:27:17 +0800

    基础控制器 随机字符生成工具包

---
+++ src/Facade/Utility/Random.php
@@ -0,0 +1,26 @@
+<?php
+namespace CloverSwoole\CloverSwoole\Facade\Utility;
+
+/**
+ * Class Random
+ * @package CloverSwoole\CloverSwoole\Facade\Utility
+ */
+class Random
+{
+    static function randStr($length)
+    {
+        mt_srand();
+        return substr(str_shuffle("abcdefghijkmnpqrstuvwxyzABCDEFGHIJKMNPQRSTUVWXYZ23456789"), 0, $length);
+    }
+    static function randNumStr($length)
+    {
+        $chars = array(
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        );
+        $password = '';
+        while (strlen($password) < $length) {
+            $password .= $chars[rand(0, 9)];
+        }
+        return $password;
+    }
+}
