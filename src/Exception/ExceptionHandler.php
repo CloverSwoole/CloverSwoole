f90f47118c64f01a2e7f9067ea3719f13ff05f79
戒尺 <itxiao6@qq.com>
2019-07-19 09:43:56 +0800

    异常捕获接管服务

--- src/Exception/ExceptionHandler.php
+++ src/Exception/ExceptionHandler.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace CloverSwoole\Exception;
+
 use CloverSwoole\Http\Response;
 
 /**
@@ -13,17 +15,22 @@ class ExceptionHandler
      * @var array
      */
     protected $exception_handler = [
-        'routeExceptionHandler'=>null
+        'routeExceptionHandler' => null,
+        'timerExceptionHandler' => null,
+        'taskExceptionHandler' => null,
+        'controllerExceptionHandler' => null
     ];
+
     public static function getInterface()
     {
         return (new static());
     }
+
     /**
      * 设置路由异常处理
-     * @param $callback
+     * @param \Closure $callback
      */
-    public function setRouteExceptionHandler($callback)
+    public function setRouteExceptionHandler(\Closure $callback)
     {
         $this->exception_handler['routeExceptionHandler'] = $callback;
     }
@@ -34,8 +41,27 @@ class ExceptionHandler
      */
     public function catchRoute(\Throwable $throwable)
     {
-        Response::getInterface() -> withContent('路由异常:'.$throwable -> getMessage());
-        Response::getInterface() -> endResponse();
+        /**
+         * 判断是否设置了自定义异常处理
+         */
+        if (!($this->exception_handler['routeExceptionHandler'] instanceof \Closure)) {
+            Response::getInterface()->withContent('路由异常:' . $throwable->getMessage());
+            Response::getInterface()->endResponse();
+            return;
+        }
+        /**
+         * 调用自定义异常处理
+         */
+        $this->exception_handler['routeExceptionHandler'](...func_get_args());
+    }
+
+    /**
+     * 修改控制器异常处理程序
+     * @param \Closure $callback
+     */
+    public function setControllerExceptionHandler(\Closure $callback)
+    {
+        $this->exception_handler['controllerExceptionHandler'] = $callback;
     }
 
     /**
@@ -44,15 +70,55 @@ class ExceptionHandler
      */
     public function catchController(\Throwable $throwable)
     {
-        Response::getInterface() -> withContent('异常:'.$throwable -> getMessage());
-        Response::getInterface() -> endResponse();
+        if (!($this->exception_handler['controllerExceptionHandler'] instanceof \Closure)) {
+            Response::getInterface()->withContent('异常:' . $throwable->getMessage());
+            Response::getInterface()->endResponse();
+            return;
+        }
+        $this->exception_handler['controllerExceptionHandler'](...func_get_args());
     }
+
+    /**
+     * 设置定时器异常处理程序
+     * @param \Closure $callback
+     */
+    public function setTimerExceptionHandler(\Closure $callback)
+    {
+        $this->exception_handler['timerExceptionHandler'] = $callback;
+    }
+
+    /**
+     * 捕获定时器异常处理程序
+     * @param \Throwable $throwable
+     */
     public function catchTimer(\Throwable $throwable)
     {
-        echo("定时器异常:".$throwable -> getMessage()."\n");
+        if (!($this->exception_handler['timerExceptionHandler'] instanceof \Closure)) {
+            echo("定时器异常:" . $throwable->getMessage() . "\n");
+            return;
+        }
+        $this->exception_handler['timerExceptionHandler'](...func_get_args());
     }
+
+    /**
+     * 设置Task异常处理程序
+     * @param \Closure $callback
+     */
+    public function setTaskExceptionHandler(\Closure $callback)
+    {
+        $this->exception_handler['taskExceptionHandler'] = $callback;
+    }
+
+    /**
+     * 捕获Task任务异常
+     * @param \Throwable $throwable
+     */
     public function catchTask(\Throwable $throwable)
     {
-        echo("任务异常:".$throwable -> getMessage()."\n");
+        if (!($this->exception_handler['taskExceptionHandler'] instanceof \Closure)) {
+            echo("任务异常:" . $throwable->getMessage() . "\n");
+            return;
+        }
+        $this->exception_handler['taskExceptionHandler'](...func_get_args());
     }
 }
